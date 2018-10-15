f440b338a1032b4bcbe78efa432f62726f502058
jeffreydn1161 <jeffreydn1161@gmail.com>
2018-10-15 12:11:08 -0400

    Create Hello_World.java

---
+++ Hello_World.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Jeffrey Norris on 10/15/2018.
+ */
+public class Hello_World {
+
+    public static void main (String[]args) {
+        System.out.println("Hello World");
+    }
+}
